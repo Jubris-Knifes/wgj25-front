a2e20486cd5b570435f9cd30ac40459f77ab1029
Juan Casanova <j.casanoval@hotmail.com>
2025-08-09 13:04:53 -0300

    cahnge fake display to cross

--- packages/app_ui/lib/src/widgets/art_card.dart
+++ packages/app_ui/lib/src/widgets/art_card.dart
@@ -2,19 +2,37 @@ import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 
 class ArtCard extends StatelessWidget {
-  const ArtCard({super.key, required this.art, this.displayFake = false});
+  const ArtCard({
+    required this.art,
+    this.displayFake = false,
+    this.onTap,
+    super.key,
+  });
 
   final Art art;
   final bool displayFake;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    final borderColor = displayFake && !art.isReal ? Colors.red : Colors.black;
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: Image.asset(
-        'assets/images/art_cards/${art.type.name}_${art.id}_${art.isReal ? 'R' : 'F'}.png',
-        package: 'app_ui',
+    final showFake = displayFake && !art.isReal;
+    return InkWell(
+      onTap: onTap,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              'assets/images/art_cards/${art.type.name}_${art.id}_${art.isReal ? 'R' : 'F'}.png',
+              package: 'app_ui',
+            ),
+          ),
+          if (showFake)
+            const Align(
+              alignment: Alignment.topRight,
+              child: Icon(Icons.close, color: Colors.red),
+            ),
+        ],
       ),
     );
   }
